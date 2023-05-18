f699d774494c5dd26d6736ea9efd79071533b4b3
Logan Thomas <logan.thomas@forsta.com>
2023-05-18 02:19:58 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import puppeteer from "puppeteer";
+
+const getQuotes = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+
+  await page.goto("http://quotes.toscrape.com/", {
+    waitUntil: "domcontentloaded",
+  });
+};
